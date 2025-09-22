00b895d7c9a27ccd04b8db3aa608f8532c9e4f44
Axnmihn <axadmihn@axnmihn.com>
2025-09-22 14:36:05 -0700

    Update index.py

--- api/index.py
+++ api/index.py
@@ -5,7 +5,7 @@ class handler(BaseHTTPRequestHandler):
     def do_GET(self):
         pi=355/113
         radius=7
-        area=('pi'*'radius'**2)
+        area=(pi*radius**2)
         self.send_response(200)
         self.send_header('Content-type','text/plain')
         self.end_headers()
