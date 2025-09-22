513ab3b160dd4614ba1cc795ec9c4299f8bfc6ca
Axnmihn <axadmihn@axnmihn.com>
2025-09-22 15:13:27 -0700

    Fix area response encoding

--- api/index.py
+++ api/index.py
@@ -6,10 +6,10 @@ class handler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header('Content-type','text/plain')
         self.end_headers()
-        self.wfile.write('Hi, this is Jongmin. Call me Mihn'.encode('utf-8'))
-        pi=355/113
-        radius=7
-        area=(pi*(radius**2))
-        int(area).encode('utf-8)
+        pi = 355 / 113
+        radius = 7
+        area = pi * (radius ** 2)
+        message = f"Hi, this is Jongmin. Call me Mihn. Area: {int(area)}"
+        self.wfile.write(message.encode('utf-8'))
 
         return
