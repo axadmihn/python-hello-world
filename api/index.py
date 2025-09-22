e1131bb07b94dcc440fd958ab254b9021e3f08df
Axnmihn <axadmihn@axnmihn.com>
2025-09-22 14:46:00 -0700

    Update index.py

--- api/index.py
+++ api/index.py
@@ -3,12 +3,12 @@ from http.server import BaseHTTPRequestHandler
 class handler(BaseHTTPRequestHandler):
 
     def do_GET(self):
-        pi=355/113
-        radius=7
-        area=(pi*radius**2)
         self.send_response(200)
         self.send_header('Content-type','text/plain')
         self.end_headers()
-        self.wfile.write('Hi, this is Jongmin. Call me Mihn'.encode('utf-8')','area')
-        
+        self.wfile.write('Hi, this is Jongmin. Call me Mihn'.encode('utf-8'))
+ pi=355/113
+        radius=7
+        area=(pi*radius**2)
+        self.wfile.write(area.encode('utf-8'))
         return
